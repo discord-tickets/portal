0d1c5267e4254d3e3a618fd4c853591d1223379b
Isaac <git@eartharoid.me>
2022-07-17 01:27:11 +0100

    Add resources

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -65,55 +65,84 @@
 			</a>
 		</div>
 	</div>
-	<div class="bg-white dark:bg-slate-700 p-4 rounded-xl shadow-sm">
-		<div
-			class="bg-gray-100 dark:bg-slate-800 p-4 rounded-xl shadow-sm flex items-center justify-center gap-4 font-semibold"
-		>
-			<img src={client.avatar} alt="" class="h-12 rounded-full" />
-			<span class="font-bold text-2xl">
-				{client.username}<span class="text-gray-500 dark:text-slate-400"
-					>#{client.discriminator}</span
-				>
-			</span>
+	<div>
+		<div class="bg-white dark:bg-slate-700 p-4 rounded-xl shadow-sm mb-4">
+			<div
+				class="bg-gray-100 dark:bg-slate-800 p-4 rounded-xl shadow-sm flex items-center justify-center gap-4 font-semibold"
+			>
+				<img src={client.avatar} alt="" class="h-12 rounded-full" />
+				<span class="font-bold text-2xl">
+					{client.username}<span class="text-gray-500 dark:text-slate-400"
+						>#{client.discriminator}</span
+					>
+				</span>
+			</div>
+			<div class="m-4 grid grid-cols-2 sm:grid-cols-3 2xl:grid-cols-5 gap-4">
+				<div>
+					<h6 class="font-semibold">Activated users</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.activatedUsers}</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Archived messages</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.archivedMessages}</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Avg. response</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.avgResponseTime}</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Categories</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.categories}</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Guilds</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.guilds}</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Avg. members</h6>
+					<p class="text-gray-500 dark:text-slate-400">
+						{Math.floor(client.stats.members / client.stats.guilds)}
+					</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Total members</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.members}</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Tags</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.tags}</p>
+				</div>
+				<div>
+					<h6 class="font-semibold">Tickets</h6>
+					<p class="text-gray-500 dark:text-slate-400">{client.stats.tickets}</p>
+				</div>
+			</div>
 		</div>
-		<div class="m-4 grid grid-cols-2 sm:grid-cols-3 2xl:grid-cols-5 gap-4">
-			<div>
-				<h6 class="font-semibold">Activated users</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.activatedUsers}</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Archived messages</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.archivedMessages}</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Avg. response</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.avgResponseTime}</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Categories</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.categories}</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Guilds</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.guilds}</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Avg. members</h6>
-				<p class="text-gray-500 dark:text-slate-400">
-					{Math.floor(client.stats.members / client.stats.guilds)}
-				</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Total members</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.members}</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Tags</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.tags}</p>
-			</div>
-			<div>
-				<h6 class="font-semibold">Tickets</h6>
-				<p class="text-gray-500 dark:text-slate-400">{client.stats.tickets}</p>
+		<div class="bg-white dark:bg-slate-700 p-4 rounded-xl shadow-sm">
+			<h2 class="font-bold text-lg mb-2  text-center">Resources</h2>
+			<div class="grid grid-cols-1 gap-4">
+				<a href="https://discordtickets.app" target="_blank">
+					<div
+						class="bg-gray-100 dark:bg-slate-800/75 p-4 rounded-xl shadow-sm link flex items-center gap-4"
+					>
+						<i class="fa-solid fa-book text-5xl" />
+						<div>
+							<p class="text-lg font-semibold">Documentation</p>
+							<p>Follow the guide and find answers</p>
+						</div>
+					</div>
+				</a>
+				<a href="https://lnk.earth/discord" target="_blank">
+					<div
+						class="bg-gray-100 dark:bg-slate-800/75 p-4 rounded-xl shadow-sm link flex items-center gap-4"
+					>
+						<i class="fa-solid fa-circle-question text-5xl" />
+						<div>
+							<p class="text-lg font-semibold">Support</p>
+							<p>Get help on Discord</p>
+						</div>
+					</div>
+				</a>
 			</div>
 		</div>
 	</div>
