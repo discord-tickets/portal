d59adb37a1240a368fc658bf60dd0d300cef66b5
Isaac <git@eartharoid.me>
2022-09-06 12:12:21 +0100

    Use node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ out: 'build' })
 	},
 	preprocess: [
 		preprocess({ postcss: true })
