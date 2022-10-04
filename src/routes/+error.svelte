596275fc6d1098e9b8846f48b2e9745cb3821b33
Isaac <git@eartharoid.me>
2022-10-04 21:58:46 +0100

    error messages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { page } from '$app/stores';
+	let message = $page.error?.message;
+	if (message?.message) message = message.message;
 </script>
 
 <div class="text-center">
@@ -7,6 +9,6 @@
 		class="bg-red-400 dark:bg-red-500 text-red-800 dark:text-red-400 bg-opacity-40 dark:bg-opacity-20 m-4 p-6 px-12 rounded-lg text-center max-w-lg inline-block"
 	>
 		<p class="font-semibold text-xl">Error {$page.status}</p>
-		<p>{$page.error?.message ?? ''}</p>
+		<p>{message ?? ''}</p>
 	</div>
 </div>
