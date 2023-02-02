dd77a9c62dc8cbc02687ceac32a50cafc4d528bd
Isaac <git@eartharoid.me>
2023-02-02 15:42:27 +0000

    fix: server-side logging

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -9,9 +9,15 @@ export async function handle({ event, resolve }) {
 /** @type {import('@sveltejs/kit').HandleServerError} */
 export function handleError({ error }) {
 	const errorId = Date.now().toString(16);
-	console.error(errorId, error);
-	let message =
-		typeof error === 'string' ? error : error?.message || JSON.stringify(error || 'Unknown error');
+
+	if (dev) console.error(errorId, error);
+
+	process?.emit('sveltekit:error', { error, errorId });
+
+	let message = typeof error === 'string'
+		? error
+		: error?.message || JSON.stringify(error || 'Unknown error');
+
 	return {
 		message: message + ` (id=${errorId})`,
 		errorId
