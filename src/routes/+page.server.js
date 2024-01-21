c1a5491aa357aef76f29051c65369ee4f8bf1382
Isaac <git@eartharoid.me>
2024-01-21 02:40:34 +0000

    fix: root redirect

--- src/routes/+page.server.js
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-/** @type {import('./$types').LayoutServerLoad} */
-export function load() {
-	redirect(307, '/settings');
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
 <script>
 	// TODO: redirect to only guild, or display selector
 </script>
+
+<svelte:head>
+	<meta http-equiv="refresh" content="0; url=/settings" />
+</svelte:head>
+
