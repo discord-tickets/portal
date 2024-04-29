2b768f327a0a978f6f58b5429c0b54f09285b9cb
Isaac <git@eartharoid.me>
2024-04-29 21:59:36 +0100

    update UI

--- src/routes/(default)/[guild]/+page.svelte
+++ src/routes/(default)/[guild]/+page.svelte
@@ -24,15 +24,23 @@
 					<a
 						href={`/${$page.params.guild}/staff`}
 						class="py-1 px-2 bg-dgrey-900/10 hover:bg-dgrey-900/20 dark:bg-dgrey-400/10 dark:hover:bg-dgrey-400/20 dark:text-dgrey-400/75 hover:dark:text-dgrey-400/100 duration-300 rounded-md"
-						>{t('common:staff_dashboard')}</a
 					>
+						<div class="flex items-center gap-2">
+							<i class="fa-solid fa-user-group"></i>
+							{t('common:staff_dashboard')}
+						</div>
+					</a>
 					{#if guild.privilegeLevel >= 2}
 						<!-- real id from guild -->
 						<a
 							href={`/settings/${guild.id}`}
 							class="py-1 px-2 bg-dgrey-900/10 hover:bg-dgrey-900/20 dark:bg-dgrey-400/10 dark:hover:bg-dgrey-400/20 dark:text-dgrey-400/75 hover:dark:text-dgrey-400/100 duration-300 rounded-md"
-							>{t('common:settings_panel')}</a
 						>
+							<div class="flex items-center gap-2">
+								<i class="fa-solid fa-gear"></i>
+								{t('common:settings_panel')}
+							</div>
+						</a>
 					{/if}
 				</div>
 			</div>
