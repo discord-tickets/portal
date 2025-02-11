1f77ff74991a0551bac124b81fd4ac14c744bdfa
Isaac <git@eartharoid.me>
2025-02-11 23:05:02 +0000

    move `/invite` redirect to app

---
+++ src/routes/(default)/invite/+page.js
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ url }) {
+	redirect(307, `/auth/login?invite&guild=${url.searchParams.get('guild') || ''}}`);
+}
+
