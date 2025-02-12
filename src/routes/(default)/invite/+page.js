c0f3a5f2b59e9248c712bc750c09aeaae57eaef8
Isaac <git@eartharoid.me>
2025-02-12 15:29:51 +0000

    fix: typo/invite

--- src/routes/(default)/invite/+page.js
+++ src/routes/(default)/invite/+page.js
@@ -2,6 +2,6 @@ import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ url }) {
-	redirect(307, `/auth/login?invite&guild=${url.searchParams.get('guild') || ''}}`);
+	redirect(307, `/auth/login?invite&guild=${url.searchParams.get('guild') || ''}`);
 }
 
